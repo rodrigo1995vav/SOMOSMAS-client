fa45f39fe3c009d52b186a28a96f0af25d38ac90
MarcosSWeis <marcosweis01@gmail.com>
2022-06-24 01:10:00 -0300

    menu's array creation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../img/home/LOGO-SOMOS-MAS.png";
+import Menu from "./Menu";
 export default function Navbar() {
+  const [menu, setMenu] = useState([
+    "Inicio",
+    "Nosotros",
+    "Novedades",
+    "Testimonios",
+    "Contacto",
+    "Contribuye",
+  ]);
   return (
     <section>
-      <nav class="navbar navbar-expand-lg p-3 border-bottom container_navbar ">
-        <div class="container-fluid">
-          <a class="navbar-brand mx-2" href="#">
+      <nav className="navbar navbar-expand-lg p-3 border-bottom container_navbar ">
+        <div className="container-fluid">
+          <a className="navbar-brand mx-2" href="">
             <img
               src={logo}
-              alt="Logo de la empresa"
+              alt="Logo ong"
               width={"40px"}
               style={{ transform: "scale(2.2)" }}
             />
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -22,85 +32,28 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div
-            class="collapse navbar-collapse  justify-content-end"
+            className="collapse navbar-collapse  justify-content-end"
             id="navbarSupportedContent"
           >
             <div className="">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link text-dark mx-5"
-                    style={{ transform: "scale(1.2)" }}
-                    aria-current="page"
-                  >
-                    Inicio
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link text-dark mx-3"
-                    style={{ transform: "scale(1.2)" }}
-                  >
-                    Nosotros
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link  text-dark mx-3"
-                    style={{ transform: "scale(1.2)" }}
-                    aria-current="page"
-                  >
-                    Novedades
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link  text-dark mx-3"
-                    style={{ transform: "scale(1.2)" }}
-                    aria-current="page"
-                  >
-                    Testimonios
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link  text-dark mx-3"
-                    style={{ transform: "scale(1.2)" }}
-                    aria-current="page"
-                  >
-                    Contacto
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    href="#"
-                    class="nav-link  text-dark mx-3"
-                    style={{ transform: "scale(1.2)" }}
-                    aria-current="page"
-                  >
-                    Contribuye
-                  </a>
-                </li>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+                {menu.length > 0 &&
+                  menu.map((section) => <Menu section={section} />)}
               </ul>
             </div>
             <div className="justify-content-end">
               <button
-                class="btn text-dark rounded-pill border border-dark mx-3"
+                className="btn text-dark rounded-pill border border-dark mx-3"
                 type="submit"
                 style={{ transform: "scale(1.2)" }}
               >
                 Login in
               </button>
               <button
-                class="btn btn-danger mx-3  rounded-pill"
+                className="btn btn-danger mx-3  rounded-pill"
                 type="submit"
                 style={{ transform: "scale(1.2)" }}
               >
