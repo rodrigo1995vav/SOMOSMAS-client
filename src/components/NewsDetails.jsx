82764fbc62520e5749c7aa6f7053b3adb44669c1
Leandro Medina <medinaleandron@gmail.com>
2022-06-27 15:29:39 -0300

    now displays the request error message

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -49,7 +49,7 @@ const NewsDetails = ({imgHeight = '40rem'}) => {
 
           : error && <div className="bg-primary text-white alert alert-danger p-5 text-start" role="alert">
                         <h4 className="alert-heading fs-1 ">Error</h4>
-                        <p className='fs-3'>La novedad no existe!!!</p>
+                        <p className='fs-3'>{error.message}</p>
                      </div>}
     </main>
         )
