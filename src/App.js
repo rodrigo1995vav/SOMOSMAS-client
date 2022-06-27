a4395f169b9918d62ec40cd71885693e521e7b59
Israelí Pérez <israeli.perez@gmail.com>
2022-06-27 15:23:31 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Footer from "./components/Footer";
-import AppRouter from "./router/AppRouter";
+import UpdateUser from "./pages/update/UpdateUser";
+// import Footer from "./components/Footer";
+// import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div>
       <h1 className="fw-bolder">hola</h1>
-        <AppRouter/>
-      <Footer />
+      <UpdateUser />
+       {/*  <AppRouter/>
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/pages/update/UpdateUser.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+
+export default function UpdateUser() {
+
+    return (
+        <>
+            <Formik
+                initialValues={{
+                    firtName: '',
+                    lastName: '',
+                    email: '',
+                    password:'',
+                    image:'',
+                    roleid:0
+                }}
+                validate={values => {
+                    let errors = {};
+                    if (!values.email) {
+                        errors.email = 'El correo es requerido';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Dirección de correo inválida';
+                    }
+                    if (!values.password) {
+                        errors.password = 'El password es requerido';
+                    } else if (values.password.length < 6) {
+                        errors.password = 'El password debe tener al menos 6 caracteres';
+                    }
+    
+                    return errors;
+                }}
+                onSubmit={(values,{resetForm})=>{
+    
+                    console.log(values)
+                    // resetForm()
+                }}
+            >{({errors}) => (
+                <Form className='form' >
+                    <h1>Editar Perfil</h1>
+
+                    <div className='separator'/>
+
+
+                    <div className='form-floating mb-3'>                
+                        <Field
+                            placeholder="tucorreo@mail.com"
+                            className='form-control'
+                            name='email'
+                            type="email"
+                        />
+                        <ErrorMessage name='email' component={ ()=>
+                            <div className='error'>{errors.email}</div>
+                        } className='error-message' />
+                    </div>
+                    <div className='form-floating mb-3'>
+                        {/* <p className='show-password' onClick={() => handleClickShowPassword()}>Mostrar</p> */}
+                        <Field
+                            id="password"
+                            name='password'
+                            className='form-control floatingPassword'
+                            // type={isShowPassword ? 'text': "password"}
+                        />
+                        <label htmlFor="password">Contraseña</label>
+                        <ErrorMessage name="password" component={ ()=>
+                            <div className='error'>{errors.password}</div>
+                        } className='error' />
+                    </div>
+                    <button type="submit" className='buttonColor'>Iniciar sesión</button>
+                </Form>
+            )}
+            </Formik>
+                        
+                        
+        </>
+      )
+}
