18b7ee06db2914a9fe5ad3f7ce9f330370957514
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-06-29 14:24:10 -0300

    Merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-//import Footer from "./components/Footer";
-import UsersConfigTable from "./components/usersConfigTable/UsersConfigTable";
-//import AppRouter from "./router/AppRouter";
+import Footer from "./components/Footer";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div>
-      <UsersConfigTable/>
+        <AppRouter/>
+        <Footer />
     </div>
   );
 }
