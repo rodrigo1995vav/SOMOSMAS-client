3ac5547fef4525ae85e39716986bd8caa79a0ecb
Mauricio Teruel <mauricioteruel98@gmail.com>
2022-06-29 21:03:09 -0300

    News page created - NewsDetail page created - Routes added to the Router

--- src/pages/news/NewsDetail.js
+++ src/pages/news/NewsDetail.js
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+import React from 'react';
 
-const News = () => {
+export const NewsDetail = () => {
 
-    //TODO: Extract the news from the API
+    //TODO: Extract the news from the API and use useParams to get the news id
     //Test variables
     const news = [
         {
@@ -13,29 +13,18 @@ const News = () => {
                 "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
                 "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
             ],
-        },
-        {
-            id: 2,
-            name: "News 2",
-            content: "Content 2",
-            image: [
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            ]
         }
     ]
 
     return (
-
         <div className="container">
-            <h1 className="title text-center my-5">Novedades</h1>
             {
                 news.map(news => (
                     <div className="card my-5" style={{ borderRadius: 15 }}>
                         <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" >
                             <div className="carousel-indicators">
                                 {news.image.map((image, index) => (<button type="button"
-                                    key={image.id}
+                                    key={image}
                                     data-bs-target="#carouselExampleIndicators"
                                     data-bs-slide-to={index}
                                     className={index === 0 ? "carousel-button active" : "carousel-button"}
@@ -45,7 +34,7 @@ const News = () => {
                                 ))}
                             </div>
                             <div className="carousel-inner" >
-                                {news.image.map((image, index) => (<div key={image.id} className={index === 0 ? "carousel-item active" : "carousel-item"}>
+                                {news.image.map((image, index) => (<div key={image} className={index === 0 ? "carousel-item active" : "carousel-item"}>
                                     <img className="d-block w-100" style={{ borderRadius: 20 }} src={image} alt={image} />
                                 </div>
                                 ))}
@@ -60,15 +49,12 @@ const News = () => {
                         <div className="card-body">
                             <h5 className="card-title text-center fs-1">{news.name}</h5>
                             <p className="card-text text-center fs-3">{news.content}</p>
-                            <Link to={`/news/${news.id}`} className="btn btn-primary fs-4" style={{ borderRadius: 30, width: 115 }}>Ver más</Link>
                         </div>
                     </div>
                 ))
             }
+
         </div>
-
-    );
-
+    )
 }
 
-export default News;
--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -3,7 +3,8 @@ import Home from '../pages/home/Home';
 import News from '../pages/news/News';
 import Login, { LoginPage } from '../pages/login/LoginPage';
 import NotFound from '../pages/notfound/NotFound';
-import ContactPage from '../pages/contact/ContactPage'
+import ContactPage from '../pages/contact/ContactPage';
+import { NewsDetail } from '../pages/news/NewsDetail';
 
 
 const PublicRoutes = () => {
@@ -11,6 +12,7 @@ const PublicRoutes = () => {
         <Routes>
             <Route exact match path='' element={<Home/>} />
             <Route path='news' element={<News/>} />
+            <Route path='news/:id' element={<NewsDetail/>} />
             <Route path='testimonials' element={<News/>} />
             <Route path='contact' element={<ContactPage/>} />
             <Route path='login' element={<LoginPage/>} />
