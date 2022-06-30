da43844436e696a9b81b52f0c2a099027a02a114
MarcosSWeis <marcosweis01@gmail.com>
2022-06-30 13:36:21 -0300

    changes to make it work

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import News from '../pages/news/News';
-import Login, { LoginPage } from '../pages/login/LoginPage';
+//import Login, { LoginPage } from '../pages/login/LoginPage';
 import NotFound from '../pages/notfound/NotFound';
 // import ContactPage from '../pages/contact/ContactPage'
 
@@ -13,7 +13,7 @@ const PublicRoutes = () => {
             <Route path='news' element={<News/>} />
             <Route path='testimonials' element={<News/>} />
           {/*   <Route path='contact' element={<ContactPage/>} /> */}
-            <Route path='login' element={<LoginPage/>} />
+           {/*  <Route path='login' element={<LoginPage/>} /> */}
             <Route path='*' element={<NotFound/>} />
         </Routes>
     );
