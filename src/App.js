00048e8bf3532ec52a3aec93cf742459fe2e410c
PellonMaison <marianomaison@hotmail.com>
2022-07-05 15:49:17 -0300

    Created Edit organization screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Footer from "./components/Footer";
 import AppRouter from "./router/AppRouter";
+import EditOrganization from "./pages/backOffice/EditOrganization";
 
 function App() {
   return (
     <div>
-      <h1 className="fw-bolder">hola</h1>
+      <h1 className="fw-bolder"></h1>
         <AppRouter/>
-      <Footer />
+        <Footer />
+      <EditOrganization/>
     </div>
   );
 }
--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import News from '../pages/news/News';
-import Login, { LoginPage } from '../pages/login/LoginPage';
+//import Login, { LoginPage } from '../pages/login/LoginPage';
 import NotFound from '../pages/notfound/NotFound';
-import ContactPage from '../pages/contact/ContactPage'
+//import ContactPage from '../pages/contact/ContactPage'
 
 
 const PublicRoutes = () => {
@@ -12,8 +12,8 @@ const PublicRoutes = () => {
             <Route exact match path='' element={<Home/>} />
             <Route path='news' element={<News/>} />
             <Route path='testimonials' element={<News/>} />
-            <Route path='contact' element={<ContactPage/>} />
-            <Route path='login' element={<LoginPage/>} />
+            {/* <Route path='contact' element={<ContactPage/>} />
+            <Route path='login' element={<LoginPage/>} /> */}
             <Route path='*' element={<NotFound/>} />
         </Routes>
     );
