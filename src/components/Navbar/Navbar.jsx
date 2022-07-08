a1acc153f6d7e1dcdd4ce34ec44b102f7629b54c
francogaliotti <francogaliotti@gmail.com>
2022-07-08 19:08:36 -0300

    cambios

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
         ]
     };
     return (
-        <section className="sticky-top ps-5" style={{backgroundColor: '#EAEBF3'}} >
+        <section className="sticky-top ps-5" style={{ backgroundColor: '#EAEBF3' }} >
             <nav className="navbar navbar-expand-lg p-3 border-bottom container_navbar ps-5">
                 <div className="container-fluid">
                     <a className="navbar-brand mx-2" href="">
@@ -73,30 +73,34 @@ export default function Navbar() {
                             </ul>
                         </div>
                         <div className="justify-content-end">
-                            <button
-                                className="btn text-dark rounded-pill border border-dark mx-3"
-                                type="submit"
-                                style={{ transform: "scale(1.2)" }}
-                                onClick={()=>{navigate('/login')}}
-                            >
-                                Log In
-                            </button>
-                            <button
-                                className="btn btn-danger mx-3  rounded-pill"
-                                type="submit"
-                                style={{ transform: "scale(1.2)" }}
-                            >
-                                Registrate
-                            </button>
+
                             {
-                                userLogged &&
-                            <button
-                                className="btn btn-danger mx-3  rounded-pill"
-                                type="submit"
-                                style={{ transform: "scale(1.2)" }}
-                            >
-                                Registrate
-                            </button>
+                                userLogged ?
+                                    <button
+                                        className="btn btn-danger mx-3  rounded-pill"
+                                        type="submit"
+                                        style={{ transform: "scale(1.2)" }}
+                                    >
+                                        Log Out
+                                    </button> :
+                                    <>
+                                        <button
+                                            className="btn text-dark rounded-pill border border-dark mx-3"
+                                            type="submit"
+                                            style={{ transform: "scale(1.2)" }}
+                                            onClick={() => { navigate('/login') }}
+                                        >
+                                            Log In
+                                        </button>
+                                        <button
+                                            className="btn btn-danger mx-3  rounded-pill"
+                                            type="submit"
+                                            style={{ transform: "scale(1.2)" }}
+                                        >
+                                            Registrate
+                                        </button>
+                                    </>
+
                             }
                         </div>
                     </div>
