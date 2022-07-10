cd5697aeaf8036f9d24c6d90ef0e70bb416e19cd
MarcosSWeis <marcosweis01@gmail.com>
2022-07-10 18:11:34 -0300

    fixed loader

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import AppRouter from "./router/AppRouter";
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
-import TableActivities from "./components/BackOffice/activities/TableActivities";
 
 function App() {
     return (
         <Provider store={store}>  
-                <TableActivities />
+                <AppRouter />
         </Provider>
     );
 }
