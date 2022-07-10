24f1502b5790823bf0bb94e8346497bd76d2edcf
Leandro Medina <medinaleandron@gmail.com>
2022-07-10 18:06:10 -0300

    Update AlertService.js

--- src/services/AlertService.js
+++ src/services/AlertService.js
@@ -64,7 +64,7 @@ export default class Alert {
     }
     
     
-    static confirmRequest({title,message},request,onSuccess){
+    static confirmRequest({title,message}, request, onSuccess){
         Swal.fire({
             title,
             text:message,
@@ -76,16 +76,16 @@ export default class Alert {
             confirmButtonColor: this.buttonConfirmColor,
             showLoaderOnConfirm: true,
             preConfirm: () => {
-              return request
+              return request()
                 .then(response => {
                   if (!response.ok) {
                     throw new Error(response.statusText)
                   }
-                  return response.json()
+                  return response.data
                 })
-                .catch(error => {
+                .catch(err => {
                   Swal.showValidationMessage(
-                    `No se pudo realizar la solicitud: ${error}`
+                    `No se pudo realizar la solicitud: ${err.response.data.message}`
                   )
                   Swal.getConfirmButton().textContent ='Reintentar'
                 })
