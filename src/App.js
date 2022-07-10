45ae1abd8aa7b9206d2c4a3d6dd246bb033167ff
MarcosSWeis <marcosweis01@gmail.com>
2022-07-10 16:50:12 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ import AppRouter from "./router/AppRouter";
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
-import TableActivities from "./components/BackOffice/activities/TableActivities";
 
 function App() {
     return (
         <Provider store={store}>
             <div>
-               {/*  <AppRouter /> */}
+                <AppRouter />
 
-<TableActivities/>
               {/*   <Footer /> */}
             </div>
         </Provider>
