bb6c3d4a6f849836f854229b1276fcdabab0268e
MarcosSWeis <marcosweis01@gmail.com>
2022-07-22 16:13:03 -0300

    page/staff

--- src/components/membersList/MemberDetail.jsx
+++ src/components/membersList/MemberDetail.jsx
@@ -6,14 +6,14 @@ export default function MemberDetail({ member, selectMember }) {
             <h1 className="text-center pt-5 pb-5"><strong>¡Nuestro staff!</strong></h1>
             <div className="containerMemberDetail ">
                 <section className="p-2">
-                    <h3 className="fs-2 "><strong>Roberto Martinez</strong></h3>
-                    <h4 className="fs-2 ">Rol que desempeña</h4>
-                    <p className="fs-3 ">{'Texto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeñaTexto con descripcion de la persona y rol que desempeña'}</p>
+                    <h3 className="fs-2 "><strong>{currentMember.name}</strong></h3>
+                    <h4 className="fs-2 ">{currentMember.Role_ong.role_ong}</h4>
+                    <p className="fs-3 ">{currentMember.description}</p>
                     <button type="button" class="btn bg-danger fs-2 mt-3 mb-3 text-light ">¡Quiero ser parte!</button>
                 </section>
 
                 <div className="containerImgMemberDetail">
-                    <img src={`https://reqres.in/img/faces/5-image.jpg`} className='renderImgMember' style={{ borderRadius: '2rem' }} alt="image member" />
+                    <img src={currentMember.image} className='renderImgMember' style={{ borderRadius: '2rem' }} alt="image member" />
                 </div>
             </div>
         </div>
