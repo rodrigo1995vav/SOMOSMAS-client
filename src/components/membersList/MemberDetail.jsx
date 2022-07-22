cb545815a1e2c2cf996f143bf0fd1382dd232ffb
MarcosSWeis <marcosweis01@gmail.com>
2022-07-22 16:28:24 -0300

    redirect to contact

--- src/components/membersList/MemberDetail.jsx
+++ src/components/membersList/MemberDetail.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
+
 export default function MemberDetail({ member, selectMember }) {
+    const navigate = useNavigate()
     const currentMember = selectMember ? selectMember : member
     return (
         <div className="containerComponentDetailMember">
@@ -8,7 +12,7 @@ export default function MemberDetail({ member, selectMember }) {
                     <h3 className="fs-2 "><strong>{currentMember.name}</strong></h3>
                     <h4 className="fs-2 ">{currentMember.Role_ong.role_ong}</h4>
                     <p className="fs-3 ">{currentMember.description}</p>
-                    <button type="button" class="btn bg-danger fs-2 mt-3 mb-3 text-light ">¡Quiero ser parte!</button>
+                    <button type="button" class="btn bg-danger fs-2 mt-3 mb-3 text-light" onClick={() => navigate('/contacto')}>¡Quiero ser parte!</button>
                 </section>
 
                 <div className="containerImgMemberDetail">
