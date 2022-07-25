ce5f10e9bea82e4c958d3b0e9356d260a392cdc5
Leandro Medina <medinaleandron@gmail.com>
2022-07-24 22:16:50 -0300

    added testimonial form and submit

--- src/pages/testimonials/TestimonialsPage.jsx
+++ src/pages/testimonials/TestimonialsPage.jsx
@@ -6,19 +6,35 @@ import { Loader } from "../../components/Loader"
 import Paginator from "../../components/Paginator"
 import TestimonialCard from "../../components/TestimonialCard"
 import { getPublic } from "../../services/apiServices"
-
-
+import TestimonialForm from "./TestimonialForm"
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/slices/users';
+import Alert from "../../services/AlertService"
 const TestimonialsPage = () =>{
 
     const [pageCount, setPageCount] =  useState(null)
     const [data,setData] = useState(null)
     const [loading,setLoading] = useState(true)
     const [error, setError] = useState(false)
+    const [form,setForm] = useState(false)
+    const {user} = useSelector(selectUser)
     
    const { page } = useParams()
    const limit = 15
   const cardsPerRow = 5
  
+
+
+
+    const handleForm = ()=>{
+        if (!user){
+           return Alert.info({title:'Necesita estar logueado para dejar un testimonio'})
+        }
+        setForm(true)
+
+    }
+
+
      
     useEffect(()=>{
         getPublic(`/testimonials/${limit}/${page}`)
@@ -42,10 +58,11 @@ const TestimonialsPage = () =>{
 
     return (
    data ? <main className=" container d-flex flex-column justify-content-center align-items-center gap-3 my-5" style={{height:'fit-content',width:'100%'}}>
+          {form&&<TestimonialForm userData={user} close={()=>setForm(false)}></TestimonialForm>}  
         <h1 className="text-center mt-5 mb-2">Testimonios</h1>
         {data.map((row,index) => <div key={`row${index}`} className=" w-md-auto w-100  h-auto m-auto d-flex flex-column d-md-inline-flex justify-content-center flex-md-row align-items-center gap-3">{row.map( testimonial => <TestimonialCard key={`testimonial${testimonial.id}`} {...testimonial}/>)}</div>)}
         <Paginator currentPage={page} pageCount ={pageCount}></Paginator>
-        <button className="btn btn-primary align-self-center align-self-md-start mt-5 px-5 py-2 fs-1 shadow text-white" style={{borderRadius:'1.5rem'}}>Agregar mi testimonio</button>
+        <button className="btn btn-primary align-self-center align-self-md-start mt-5 px-5 py-2 fs-1 shadow text-white" onClick={handleForm} style={{borderRadius:'1.5rem'}}>Agregar mi testimonio</button>
         <button className="btn btn-white align-self-center align-self-md-start my-2 px-5 py-2 fs-1 shadow border border-dark" style={{borderRadius:'1.5rem'}}>Ir al inicio</button>
     </main>
     : error && <ErrorSign error={error}></ErrorSign>
--- src/store/slices/contact/index.js
+++ src/store/slices/contact/index.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import Alert from "../../../services/AlertService";
 import { postPublic } from "../../../services/apiServices";
 
 
@@ -36,15 +37,17 @@ export default contactSlice.reducer
 
 export const { saveInfoContact } = contactSlice.actions
 
-export const contactUs = ({ email, name, message }) => {
+export const contactUs = ({ email, name, message },onSuccess) => {
     return (dispatch) => {
         dispatch(saveInfoContact({ loading: true, responseContact: null }))
         postPublic(`${process.env.REACT_APP_PUBLIC_URL_API}/contacts?email=${email}&name=${name}&message=${message}`)
             .then(({ data }) => {
                 dispatch(saveInfoContact({ loading: false, responseContact: data }))
+                onSuccess()
             }).catch((err) => {
                 console.log(err)
                 dispatch(saveInfoContact({ loading: false, error: { errorState: true, error: err.message } }))
+                Alert.error({title:err.name , message:err.message})
             });
     }
 }  
