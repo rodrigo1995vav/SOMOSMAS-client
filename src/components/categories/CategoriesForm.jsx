38c93fcaf64f46d1e01de20d3cc31740e650f60d
Leandro Medina <medinaleandron@gmail.com>
2022-07-26 18:26:48 -0300

    Revert "merge"
    This reverts commit e8d8a749b3ca6e5272894ef6fe58f35cb7e8826c, reversing
    changes made to 47202796a9da1573b955481cb451b194edd5a79b.

--- src/components/categories/CategoriesForm.jsx
+++ src/components/categories/CategoriesForm.jsx
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 import Alert from '../../services/AlertService';
 import { postPrivate, putPrivate } from '../../services/apiServices';
-=======
-
-import { Formik, Form } from 'formik';
-import { useParams } from 'react-router-dom';
-import * as Yup from 'yup';
-import Alert from '../../services/AlertService';
-import { postPrivate, putPrivate } from '../../services/apiServices';
-
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
 import { CustomTextArea } from '../TextArea';
 import { CustomTextInput } from '../TextInput';
 
 
-<<<<<<< HEAD
 const CategoriesForm = ({ initialValues, selfClose, setCategories }) => {  
-=======
-const CategoriesForm = ({ initialValues }) => {
-
-  const { id } = useParams();
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
 
   // It returns the validation schema object used by Formik
   const yupValidationSchema = () => ({
@@ -40,10 +24,7 @@ const CategoriesForm = ({ initialValues }) => {
     let response, message;
 
     if( initialValues ){
-<<<<<<< HEAD
       const id = initialValues.id
-=======
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
       response = await putPrivate( `/categories/`, id, values );
       message  = "Categoria actualizada exitosamente"
     }else{
@@ -51,7 +32,6 @@ const CategoriesForm = ({ initialValues }) => {
       message  = "Categoria creada exitosamente"
     }
 
-<<<<<<< HEAD
     if( response.data === 1 ){
       setCategories( categories =>{
         const updatedCategories = categories.map( cat => {
@@ -64,14 +44,10 @@ const CategoriesForm = ({ initialValues }) => {
         return updatedCategories;
       } )
 
-=======
-    if( response.data.categoryCreated || response.data === 1 ){
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
       Alert.success({
         title: 'Operación exitosa',
         message 
       })
-<<<<<<< HEAD
     }else if( response.data.categoryCreated ){
 
       setCategories(categories => ([ ...categories, values ]) )
@@ -81,8 +57,6 @@ const CategoriesForm = ({ initialValues }) => {
         message 
       })
 
-=======
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
     }else{
       Alert.error({
         title: "Error",
@@ -90,7 +64,6 @@ const CategoriesForm = ({ initialValues }) => {
       })
     }
 
-<<<<<<< HEAD
     handleSelfClose();
 
   }
@@ -145,45 +118,6 @@ const CategoriesForm = ({ initialValues }) => {
           </Formik>
       </div>
 
-=======
-  }
-
-  return (
-    <div style={{ padding: '3rem', maxWidth: '90rem' }}>
-       <h1 style={{ fontSize: '2.8rem', marginBottom:'1rem' }}> Categorías </h1>
-       <Formik
-          initialValues={ initialValues || { name:'', description:'' } }
-          onSubmit={ values => handleOnSubmit( values )}
-          validationSchema={ Yup.object(yupValidationSchema()) }
-        >
-          {
-            () => (
-              <Form>
-                <CustomTextInput 
-                  placeholder="Nombre" 
-                  name="name" 
-                  className="text-input_324af" 
-                /> 
-
-                <CustomTextArea 
-                  placeholder="Descripción" 
-                  name="description" 
-                  className="text-area_324af"
-                />
-
-                <button 
-                  type="submit" 
-                  className="btn btn-primary py-2 px-4 mt-4 text-white fs-3 d-inline-block"
-                  style={ { borderRadius: '6px' } }
-                >
-                  Enviar
-                </button>
-              </Form>
-            )
-          }
-        </Formik>
-    </div>
->>>>>>> cea4c65b514390119c906a1100c11439877077ed
   )
 }
 
